bf287249449434cc2181829b0c438aff7996c085
guhan0000 <guhanm0000@gmail.com>
2026-08-21 17:53:41 +0530

    interface lists

--- interfaces/interface1.ts
+++ interfaces/interface1.ts
@@ -18,3 +18,11 @@ const user: { userId: number; userName: string } = {
   userId: 1,
   userName: "guhan",
 };
+console.log(prod.isProdAvail);
+
+// multiple prods product list using the Product interface
+const products: Product[] = [
+  { prodId: 1, prodName: "Shoe", prodPrice: 400, isProdAvail: false },
+  { prodId: 2, prodName: "Phone", prodPrice: 44400, isProdAvail: true },
+];
+console.log(products);
